c343bfdc6bfbbf3cf66645a2be870b71de47c871
1585832651 <103298552+1585832651@users.noreply.github.com>
2022-12-06 19:30:27 +0800

    Delete ASSIGNMENT1 directory

--- ASSIGNMENT1/RMSE.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import math
-def RMSE_loss(W,x,y):
-    """向量化操作，numpy的优势就体现出来了"""
-    num=x.shape[0]
-    loss=np.sqrt(np.sum((np.dot(x,W.T)-y)**2)/num)
-    grad=np.zeros(W.shape)
-    grad+=1/(2*np.sqrt(abs(np.sum((y-np.dot(x,W.T))*x/num)*2))) if np.sum((y-np.dot(x,W.T))*x/num)>0 else -1/(2*np.sqrt(abs(np.sum((y-np.dot(x,W.T))*x/num)*2)))
-    return loss,grad
